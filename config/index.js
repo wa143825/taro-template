1d201e4772e823ad5168522ab2ec43952a13ab4c
revive <wa143825@outlook.com>
2022-08-23 11:32:27 +0800

    feat:整体升级

--- config/index.js
+++ config/index.js
@@ -1,41 +1,41 @@
 /*
  * @Author: Re_Vive
- * @LastEditTime: 2022-04-08 11:03:24
+ * @LastEditTime: 2022-08-23 11:15:54
  * @Description: 头部注释配置模板
  */
-import path from "path";
+import path from 'path'
 
 const config = {
-  projectName: "huao",
-  date: "2022-2-16",
+  projectName: 'template',
+  date: '2022-8-23',
   designWidth: 750,
   deviceRatio: {
     640: 2.34 / 2,
     750: 1,
     828: 1.81 / 2,
   },
-  sourceRoot: "src",
-  outputRoot: "dist",
+  compiler: {
+    type: 'webpack5',
+    prebundle: {
+      enable: true,
+    },
+  },
+  // 持久化缓存配置
+  cache: {
+    enable: true,
+  },
+  sourceRoot: 'src',
+  outputRoot: 'dist',
   plugins: [
     [
-      "taro-plugin-tailwind",
+      'taro-plugin-tailwind',
       {
         scan: {
-          dirs: ["./src"],
-          exclude: ["dist/**/*"],
+          dirs: ['./src'],
+          exclude: ['dist/**/*'],
         },
       },
     ],
-		// [
-		// 	'@tarojs/plugin-inject', {
-		// 		components: {
-		// 			Input: {
-		// 				'data-hint': ""
-		// 			}
-		// 		}
-
-		// 	}
-		// ]
   ],
 
   defineConstants: {},
@@ -43,7 +43,7 @@ const config = {
     patterns: [],
     options: {},
   },
-  framework: "react",
+  framework: 'react',
   mini: {
     postcss: {
       pxtransform: {
@@ -59,15 +59,15 @@ const config = {
       cssModules: {
         enable: true, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
-          namingPattern: "module", // 转换模式，取值为 global/module
-          generateScopedName: "[name]__[local]___[hash:base64:5]",
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
         },
       },
     },
   },
   h5: {
-    publicPath: "/",
-    staticDirectory: "static",
+    publicPath: '/',
+    staticDirectory: 'static',
     postcss: {
       autoprefixer: {
         enable: true,
@@ -76,20 +76,20 @@ const config = {
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
-          namingPattern: "module", // 转换模式，取值为 global/module
-          generateScopedName: "[name]__[local]___[hash:base64:5]",
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
         },
       },
     },
   },
   alias: {
-    "@": path.resolve(__dirname, "..", "src"),
+    '@': path.resolve(__dirname, '..', 'src'),
   },
-};
+}
 
 module.exports = function (merge) {
-  if (process.env.NODE_ENV === "development") {
-    return merge({}, config, require("./dev"));
+  if (process.env.NODE_ENV === 'development') {
+    return merge({}, config, require('./dev'))
   }
-  return merge({}, config, require("./prod"));
-};
+  return merge({}, config, require('./prod'))
+}
